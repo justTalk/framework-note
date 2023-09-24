4df29b2df192dd34b8e1390b72f232956aa0f4d3
mingming.liu@quvideo.com <mingming.liu@quvideo.com>
2023-09-24 15:25:11 +0800

    (feat):WA Stciker 隐私协议

--- app/src/main/java/com/example/framework_note/WheelActivity.kt
+++ app/src/main/java/com/example/framework_note/WheelActivity.kt
@@ -2,6 +2,8 @@ package com.example.framework_note
 
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.FontRequest
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.WheelData
 import kotlinx.android.synthetic.main.activity_wheel.iv_play
@@ -12,7 +14,13 @@ class WheelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wheel)
-
+        // 将当前 code 转换为 16 进制数
+        val hex = Integer.parseInt("1F925", 16)
+// 将当前 16 进制数转换成字符数组
+        val chars = Character.toChars(hex)
+// 将当前字符数组转换成 TextView 可加载的 String 字符串
+        val mEmojiString = String(chars)
+        findViewById<TextView>(R.id.emoji).setText(mEmojiString)
         val arrayListOf = arrayListOf<WheelData>()
         arrayListOf.add(WheelData(0,Color.parseColor("#69AEFF"), ""))
         arrayListOf.add(WheelData(1,Color.parseColor("#FFFFFF"), ""))
